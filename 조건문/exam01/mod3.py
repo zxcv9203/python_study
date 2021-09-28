a38a802da1c859d436df741791c73803c90c6380
황지원 <41350477+HWNAGJIWON@users.noreply.github.com>
2021-09-28 13:47:41 +0900

    Create mod3.py

---
+++ 조건문/exam01/mod3.py
@@ -0,0 +1,7 @@
+number = 15
+if number % 3 == 0: #number가 3의 배수인지 확인합니다.
+    print("{}는 3의 배수입니다.".format(number))#이 코드는 실행됩니다.
+
+number = 16
+if number % 3 == 0: #number가 3의 배수인지 확인합니다.
+    print("{}는 3의 배수입니다.".format(number))#이 코드는 실행되지 않습니다.
